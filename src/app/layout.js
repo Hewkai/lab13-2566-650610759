59029e3a459a950abbe36e91bd519795afa52dbd
Hewkai <fkgthnotv@gmail.om>
2023-08-07 15:40:58 +0700

    lab13

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Nattida Phaiboontanasan"
+            studentId="650610759"
           />
         </div>
       </body>
